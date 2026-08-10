d60b114e44834dd715b08fdd2a4de50bfe7a5414
Riccardo Rigutini <47950599+richardrigutins@users.noreply.github.com>
2026-08-10 23:58:58 +0200

    fix: allow sentence-case commit subjects for dependabot compatibility

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // allow sentence-case subjects (e.g. dependabot's "Bump x from y to z"); still blocks start/pascal/upper-case
+    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+  },
 };
